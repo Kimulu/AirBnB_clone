06581b4925876ee3f35e978bef006c2d63365dd3
kimulu <kimulumichael@gmail.com>
2024-03-09 02:20:04 -0800

    modify base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class BaseModel:
-	"""This class represents the BaseModel of the
+    """This class represents the BaseModel of the
      AirBnBClone Project
 
      Args:
@@ -18,34 +18,32 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, tform))
+                    self.__dict__[key] = datetime.strptime(value,tform)
                 else:
-                    setattr(self, key, value)
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-
     def __str__(self):
-    	"""When called, it returns the string representation
-    	of the Instance of the BaseModel"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """When called, it returns the string representation
+        of the Instance of the BaseModel"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-    	"""This method updatse the updated_at with the current datetime."""
+        """This method updatse the updated_at with the current datetime."""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-    	"""
-    	Returns the Dictionary of the instance of the BaseModel.
-
-    	It includes the key/value pair of __class__ representing the 
-    	class name of the object.
-    	"""
+        """
+        Returns the Dictionary of the instance of the BaseModel.
+        It includes the key/value pair of __class__ representing the
+        class name of the object.
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-

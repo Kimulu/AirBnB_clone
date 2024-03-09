22f509a3818439ed30026522e55cdd4659c89daf
kimulu <kimulumichael@gmail.com>
2024-03-09 03:00:49 -0800

    change bas_mode.py

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    self.__dict__[key] = datetime.strptime(value,tform)
+                    self.__dict__[key] = datetime.strptime(value, tform)
                 else:
                     self.__dict__[key] = value
         else:
